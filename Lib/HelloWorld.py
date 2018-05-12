7dc6a0f4b13280745c1138883b9dc316413d43b7
Pulkit Singh <pulkitsingh01@gmail.com>
2018-05-12 15:34:49 +0530

    Single Neuron

--- Lib/HelloWorld.py
+++ Lib/HelloWorld.py
@@ -5,27 +5,32 @@ import numpy as np
 import os
 
 
-def train_gate(TEST=False, EPOCH=10000,
-               LEARNING_RATE=0.02, ENABLE_TENSORBOARD=False,
-               TB_PATH="../logs/1/train", MODEL_PATH_ARGS=(4000, 80816)):
+def train_gate(TEST=False,
+               EPOCH=10000,
+               LEARNING_RATE=0.02,
+               BATCH_SIZE=4,
+               SHAPE=[2, 1],
+               InX=[[1, 1], [1, 0], [0, 1], [0, 0]],
+               OutX=[1, 0, 0, 0],
+               ENABLE_TENSORBOARD=False,
+               MODEL_PATH_ARGS=(4000, 80816)):
     MODEL_PATH = "../model/model_%s_%s.ckpt"
+    TB_PATH = "../logs/1/train"
 
-    InX = [[1, 1], [1, 0], [0, 1], [0, 0]]
-    OutX = [1, 0, 0, 0]
+    x = tf.placeholder(dtype=tf.float32, shape=SHAPE)
 
-    x = tf.placeholder(dtype=tf.float32, shape=[2, 1])
-
-    w1 = tf.get_variable("w1", dtype=tf.float32, shape=[2, 1], initializer=tf.constant_initializer([[1.0], [2.0]]))
-    b1 = tf.get_variable("b1", dtype=tf.float32, shape=[2, 1], initializer=tf.constant_initializer([2.0]))
+    w1 = tf.get_variable("w1", dtype=tf.float32, shape=SHAPE, initializer=tf.constant_initializer([[1.0], [2.0]]))
+    b1 = tf.get_variable("b1", dtype=tf.float32, shape=SHAPE, initializer=tf.constant_initializer([2.0]))
     o1 = tf.add(tf.matmul(tf.transpose(x), w1), b1)
 
-    w2 = tf.get_variable("w2", dtype=tf.float32, shape=[2, 1], initializer=tf.constant_initializer([[3.0], [8.0]]))
-    b2 = tf.get_variable("b2", dtype=tf.float32, shape=[2, 1], initializer=tf.constant_initializer([14.0]))
+    w2 = tf.get_variable("w2", dtype=tf.float32, shape=SHAPE, initializer=tf.constant_initializer([[3.0], [8.0]]))
+    b2 = tf.get_variable("b2", dtype=tf.float32, shape=SHAPE, initializer=tf.constant_initializer([14.0]))
     o2 = tf.add(tf.matmul(tf.transpose(x), w2), b2)
 
-    w = tf.get_variable("w", dtype=tf.float32, shape=[2, 1], initializer=tf.constant_initializer([[2.0], [1.0]]))
-    b = tf.get_variable("b", dtype=tf.float32, shape=[2, 1], initializer=tf.constant_initializer([-4.0]))
-    oRaw = tf.sigmoid(tf.add(tf.add(tf.multiply(w[0], o1), tf.multiply(w[1], o2)), b))
+    w = tf.get_variable("w", dtype=tf.float32, shape=SHAPE, initializer=tf.constant_initializer([[2.0], [1.0]]))
+    b = tf.get_variable("b", dtype=tf.float32, shape=SHAPE, initializer=tf.constant_initializer([-4.0]))
+    # oRaw = tf.sigmoid(tf.add(tf.add(tf.multiply(w[0], o1), tf.multiply(w[1], o2)), b))
+    oRaw = tf.sigmoid(tf.add(tf.matmul(tf.transpose(x), w1), b1))
     o = tf.cond(oRaw[0][0] > 0, lambda: tf.divide(oRaw[0][0], oRaw[0][0]), lambda: tf.subtract(oRaw[0][0], oRaw[0][0]))
 
     oExpected = tf.placeholder(dtype=tf.float32, shape=[1])
@@ -68,30 +73,30 @@ def train_gate(TEST=False, EPOCH=10000,
 
         for i in range(EPOCH):
             errorTotal = 0.0
-            for gate_state in range(4):
+            for batch_itr in range(BATCH_SIZE):
                 tb_merge = None
                 if TEST:
-                    oO, errorO, wO, bO, xO \
+                    oO, errorO, wO, bO, oO2, xO \
                         = sess.run(
-                        [oRaw, error, w2, b2, x],
+                        [oRaw, error, w2, b2, o2, x],
                         feed_dict={
-                            x: np.expand_dims(np.array(InX[gate_state]),
+                            x: np.expand_dims(np.array(InX[batch_itr]),
                                               axis=1),
                             oExpected: np.expand_dims(
-                                np.array(OutX[gate_state]),
+                                np.array(OutX[batch_itr]),
                                 axis=0)
                         })
                 else:
                     tb_merge = tf.summary.merge_all()
 
-                    trainO, oO, errorO, wO, bO, xO \
+                    trainO, oO, errorO, wO, bO, oO2, xO \
                         = sess.run(
-                        [train, oRaw, error, w2, b2, x],
+                        [train, oRaw, error, w2, b2, o2, x],
                         feed_dict={
-                            x: np.expand_dims(np.array(InX[gate_state]),
+                            x: np.expand_dims(np.array(InX[batch_itr]),
                                               axis=1),
                             oExpected: np.expand_dims(
-                                np.array(OutX[gate_state]),
+                                np.array(OutX[batch_itr]),
                                 axis=0)
                         })
 
@@ -100,10 +105,11 @@ def train_gate(TEST=False, EPOCH=10000,
                     tb_summary = sess.run(tb_merge)
 
                 if ENABLE_TENSORBOARD:
-                    tb_writer.add_summary(tb_summary, gate_state)
+                    tb_writer.add_summary(tb_summary, batch_itr)
 
                 if 0 == i % (EPOCH / 10):
                     print("x: " + str(xO))
+                    # print("o2: " + str(oO2))
                     print("o: " + str(oO[0][0]))
                     # print("error: " + str(errorO[0]))
                     errorTotal += errorO[0]
@@ -118,6 +124,9 @@ def train_gate(TEST=False, EPOCH=10000,
                     path = os.path.normpath(MODEL_PATH % (i, randNum))
                     saver.save(sess, path)
 
+
 if __name__ == "__main__":
-    # train_gate(TEST=False)
-    train_gate(TEST=True, MODEL_PATH_ARGS=(4000, 80816))
+    train_gate(SHAPE=[3, 1],
+               InX=[[1, 1, 1], [1, 0, 1], [0, 1, 1], [0, 0, 1]],
+               OutX=[1, 0, 0, 0])
+    # train_gate(TEST=True, MODEL_PATH_ARGS=(4000, 80816))
